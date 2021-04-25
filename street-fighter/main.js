4923d73d371268cf5a7b929b57ab7f61bff8666e
xinyingchua <79414040+xinyingchua@users.noreply.github.com>
2021-04-26 00:49:53 +0800

    linked health bar accordingly when damage is dealt

--- street-fighter/main.js
+++ street-fighter/main.js
@@ -5,7 +5,37 @@ window.onload = function () {
   let startButton = document.querySelector("#start-button");
   let instructionPage = document.querySelector("#instruction-page");
   let gamePage = document.querySelector("#landing-bg");
-  let arrowContainer = document.querySelector(".container-arrow");
+  let arrowDiv = document.querySelectorAll(".arrowkey img");
+  let p1HealthBar = document.querySelector(".player-1 .progress-bar")
+//   p1HealthBar.style.width = this.health
+  let p2HealthBar = document.querySelector(".player-2 .progress-bar")
+
+
+// CREATING CLASS FOR BOTH PLAYERS
+
+class Player {
+    name = ""
+    health = 100
+
+    constructor(name, health) {
+    this.name = name
+    this.health = health
+    }
+
+inflictDamage(opponent) {
+    if (opponent.health !== 0) {
+        opponent.health -= 20
+        console.log(opponent.health)
+    }
+    else if (opponent.health ===0) {
+        alert("K.O.")
+    }
+}
+}
+
+let player1 = new Player ("Player 1", 100)
+let player2 = new Player ("Player 2", 100)
+
 
 let arrowArr = ['up', 'up', 'down', 'down', 'left', 'left', 'right', 'right'];
 //   let arrowArr = ['up', 'down', 'left', 'right'];
@@ -15,8 +45,8 @@ let roundNum = 1;
 // Creating empty array for both players' input
 let player1Inputs = []
 let player2Inputs = []
-let player1gameClicks = 0
-let player2gameClicks = 0
+let player1GameClicks = 0
+let player2GameClicks = 0
 
 const player1KeyRef = {
     KeyW: 'up',
@@ -57,43 +87,79 @@ const player2KeyRef = {
     // Player1's Input
        if (event.code == "KeyW" || event.code == "KeyS" || event.code == "KeyA" || event.code == "KeyD" ) {
            player1Inputs.push(player1KeyRef[event.code])
-           player1gameClicks += 1
-           console.log('gameclicks is', player1gameClicks)
-           highlightMove(player1Inputs)
+           player1GameClicks += 1
+           console.log('gameclicks is', player1GameClicks)
        }
 
-    // Validation Check if Key input = Arrow Displayed
-       if(player1gameClicks === 8) {
+
+    // checkWinConditions(player1Inputs);
+
             for(let i = 0; i<player1Inputs.length; i++) {
-                if(player1Inputs[i] === arrowArr[i]){
-                    console.log("p1 complete")
-                    nextRound();
+                if(player1Inputs[i] !== arrowArr[i]) {
+                    resetPlayer1();
                 }
 
+                else if(player1Inputs[i] === arrowArr[i]) {
+
+                    if(player1GameClicks === 8) {
+                    console.log("P1 complete")
+                    // alert(`player1 won`)
+                    nextRound();
+                    player1.inflictDamage(player2)
+                    p2HealthBar.style.width = `${player2.health}%`
+                    // console.log(player1.health)
+                        
+                    }
+                    else if(player1GameClicks < 8) {
+                        arrowDiv[i].style.border = "5px solid white"
+        
+                    }   
             }
         }
+
 
     // Player2's Input
-       if (event.code == "ArrowUp" || event.code == "ArrowDown" || event.code == "ArrowLeft" || event.code == "ArrowRight" ) {
+       if(event.code == "ArrowUp" || event.code == "ArrowDown" || event.code == "ArrowLeft" || event.code == "ArrowRight" ) {
            player2Inputs.push(player2KeyRef[event.code])
         //    console.log(player2Inputs)
-            player2gameClicks += 1
-           console.log('gameclicks is', player2gameClicks)
-           
-
+            player2GameClicks += 1
+            console.log('gameclicks is', player2GameClicks)
 
        }
-       if(player2gameClicks === 8) {
-        for(let i = 0; i<player2Inputs.length; i++) {
-            if(player2Inputs[i] === arrowArr[i]){
-                console.log("P2 complete")
-                nextRound();
 
-            }
+// Validation Check if Key input = Arrow Displayed
+// If key input is not equal to arrow displayed --> reset player's input
+// If all key inputs matches arrow displayed 100% = Player wins & move on to next round
 
+       for(let j = 0; j<player2Inputs.length; j++) {
+        
+        if(player2Inputs[j] !== arrowArr[j]) {
+            resetPlayer2();
         }
-    }
-   }
+        else if(player2Inputs[j] === arrowArr[j]) {
+
+            if(player2GameClicks === 8) {
+            console.log("P2 complete")
+            nextRound();
+            player2.inflictDamage(player1)
+            p1HealthBar.style.width = `${player1.health}%`
+            // console.log(player1.health)
+            // alert(`player2 won`)
+                
+            }
+            else if(player2GameClicks < 8) {
+                arrowDiv[j].style.border = "5px solid yellow"
+
+            }   
+        }
+
+
+}
+}
+
+
+        
+
 
 // ..FUNCTIONS FUNCTIONS FUNCTIONS..
 
@@ -135,25 +201,74 @@ function createArrow() {
 function nextRound() {
     player1Inputs = [];
     player2Inputs = [];
-    player1gameClicks =0
-    player2gameClicks = 0
-    gameClicks = 0;
+    player1GameClicks = 0
+    player2GameClicks = 0
+    roundNum += 1
     createArrow()
+    for (let i =0; i<arrowDiv.length; i++) {
+    arrowDiv[i].style.border ="none"
+    }
+
 }
 
-// Validation key inputs validation 
+// Resetting player's input/ gameclicks when they make a wrong move
+function resetPlayer1() {
+    player1Inputs = [];
+    player1GameClicks = 0
+    for (let i =0; i<arrowDiv.length; i++) {
+        arrowDiv[i].style.border ="none"
+    }
+}
 
-// function highlightMove(arr) {
-//         for(let i = 0; i<arr.length; i++) {
-//             if(arr[i] === arrowArr[i]){
-//                 arrowArr[i].style.border = "10px solid green"
+function resetPlayer2() {
+    player2Inputs = [];
+    player2GameClicks = 0
+    for (let i =0; i<arrowDiv.length; i++) {
+    arrowDiv[i].style.border ="none"
+    }
+}
+
+
+
+
+
+// CODES NOT WORKING
+
+// function checkWinConditions2(input, gameClicks){
+//     for(let j = 0; j<input.length; j++) {
+
+//         if(input[j] !== arrowArr[j]) {
+//             // resetPlayer();
+//             input = [];
+//             gameClicks = 0
+//             for (let i =0; i<arrowDiv.length; i++) {
+//             arrowDiv[i].style.border ="none"
+//             }
+// }
+//         else if(input[j] === arrowArr[j]) {
+
+//             if(gameClicks === 8) {
+//             console.log("P2 complete")
+//             nextRound();
+                
+//             }
+//             else if(gameClicks < 8) {
+//                 arrowDiv[j].style.border = "10px solid yellow"
+
+//             }   
+//         }
+
 
 // }
-//         }
+// }
+
+// function resetPlayer(input, gameClicks) {
+//     input = [];
+//     gameClicks = 0
+//     for (let i =0; i<arrowDiv.length; i++) {
+//     arrowDiv[i].style.border ="none"
 //     }
-
-
-// };
+// }
 
 
 // EXPERIMENTAL CODES
